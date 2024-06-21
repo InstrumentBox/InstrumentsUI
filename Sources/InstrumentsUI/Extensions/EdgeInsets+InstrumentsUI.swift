d15ad7a7008d5e32de3767e18260467aac9ea1b1
Aleksei Zaikin <aleksi.zaikin@gmail.com>
2024-06-21 08:36:56 +0300

    Add extensions to create edge insets in a convenient way

--- Sources/InstrumentsUI/Extensions/EdgeInsets+InstrumentsUI.swift
+++ Sources/InstrumentsUI/Extensions/EdgeInsets+InstrumentsUI.swift
@@ -25,24 +25,78 @@
 import UIKit
 
 extension UIEdgeInsets {
-   /// Creates and returns a new `UIEdgeInsets` object with equal top/bottom and left/right values.
+   /// Creates and returns a new `UIEdgeInsets` object with the same value for each side.
+   ///
    /// - Parameters:
-   ///   - v: Top and bottom inset.
-   ///   - h: Left and right inset.
+   ///   - value: Inset value.
    /// - Returns: Newly created `UIEdgeInsets` object.
-   public static func symmetric(v: CGFloat, h: CGFloat) -> UIEdgeInsets {
+   public static func all(_ value: CGFloat) -> UIEdgeInsets {
+      UIEdgeInsets(top: value, left: value, bottom: value, right: value)
+   }
+
+   /// Creates and returns a new `UIEdgeInsets` object with equal top/bottom and left/right values.
+   ///
+   /// - Parameters:
+   ///   - v: Top and bottom inset. Defaults to 0.
+   ///   - h: Left and right inset. Defaults to 0.
+   /// - Returns: Newly created `UIEdgeInsets` object.
+   public static func symmetric(v: CGFloat = 0.0, h: CGFloat = 0.0) -> UIEdgeInsets {
       UIEdgeInsets(top: v, left: h, bottom: v, right: h)
+   }
+   
+   /// Creates and returns a new `UIEdgeInsets` object with given values.
+   ///
+   /// - Parameters:
+   ///   - top: Top inset. Defaults to 0.
+   ///   - left: Left inset. Defaults to 0.
+   ///   - bottom: Bottom inset. Defaults to 0.
+   ///   - right: Right inset. Defaults to 0.
+   /// - Returns: New create `UIEdgeInsets` object.
+   public static func insets(
+      top: CGFloat = 0.0,
+      left: CGFloat = 0.0,
+      bottom: CGFloat = 0.0,
+      right: CGFloat = 0.0
+   ) -> UIEdgeInsets {
+      UIEdgeInsets(top: top, left: left, bottom: bottom, right: right)
    }
 }
 
 extension NSDirectionalEdgeInsets {
+   /// Creates and returns a new `NSDirectionalEdgeInsets` object with the same value for each side.
+   ///
+   /// - Parameters:
+   ///   - value: Inset value.
+   /// - Returns: Newly created `NSDirectionalEdgeInsets` object.
+   public static func all(_ value: CGFloat) -> NSDirectionalEdgeInsets {
+      NSDirectionalEdgeInsets(top: value, leading: value, bottom: value, trailing: value)
+   }
+
    /// Creates and returns a new `NSDirectionalEdgeInsets` object with
    /// equal top/bottom and left/right values.
+   ///
    /// - Parameters:
-   ///   - v: Top and bottom inset.
-   ///   - h: Leading and trailing inset.
+   ///   - v: Top and bottom inset. Defaults to 0.
+   ///   - h: Leading and trailing inset. Defaults to 0.
    /// - Returns: Newly created `NSDirectionalEdgeInsets` object.
-   public static func symmetric(v: CGFloat, h: CGFloat) -> NSDirectionalEdgeInsets {
+   public static func symmetric(v: CGFloat = 0.0, h: CGFloat = 0.0) -> NSDirectionalEdgeInsets {
       NSDirectionalEdgeInsets(top: v, leading: h, bottom: v, trailing: h)
+   }
+
+   /// Creates and returns a new `NSDirectionalEdgeInsets` object with given values.
+   ///
+   /// - Parameters:
+   ///   - top: Top inset. Defaults to 0.
+   ///   - leading: Leading inset. Defaults to 0.
+   ///   - bottom: Bottom inset. Defaults to 0.
+   ///   - trailing: Trailing inset. Defaults to 0.
+   /// - Returns: New create `NSDirectionalEdgeInsets` object.
+   public static func insets(
+      top: CGFloat = 0.0,
+      leading: CGFloat = 0.0,
+      bottom: CGFloat = 0.0,
+      trailing: CGFloat = 0.0
+   ) -> NSDirectionalEdgeInsets {
+      NSDirectionalEdgeInsets(top: top, leading: leading, bottom: bottom, trailing: trailing)
    }
 }
